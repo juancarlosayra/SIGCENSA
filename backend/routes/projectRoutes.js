a364235a62e5d9f464f5058e110ed2aff3859a39
Ayzan <jcarlosayrab@gmail.com>
2025-06-26 12:41:52 +0200

    [FIX] Changes in package.json and temporary disabled authentication with jwt

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { createProject } = require('../controllers/projectController');
-const authJwt = require('../middleware/authJwt');
+// const authJwt = require('../middleware/authJwt');
 
 const router = express.Router();
 
-router.post('/', authJwt, createProject);
+// router.post('/', authJwt, createProject);
 
 module.exports = router;
+
+// Quitar authJwt por ahora
+router.post('/', createProject); // Sin authJwt
+
