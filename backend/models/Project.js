ffcd4a85d09062d9a8796260a4d2fd57b011b40f
Ayzan <jcarlosayrab@gmail.com>
2025-07-23 16:41:21 +0200

    [WIP] Adding fields to Project model

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -5,16 +5,40 @@ const projectSchema = new mongoose.Schema({
   program_name: {type: String, required: true},
   project_code: {type: Number, required: true},
   project_name: {type: String, required: true},
-  project_classification: {type: String, enum:["Investigación Básica","Investigación Aplicada","Desarrollo Experimental","Innovación"]},
+  project_classification: {type: String, 
+                          enum:["Investigación Básica","Investigación Aplicada","Desarrollo Experimental","Innovación"]},
   fields_of_knowledge: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'knowledgeField',
     required: true,
   }],
+  priority:{type: String, required: true},//check with queen ai ->
+  priority_objectives: {type: Text, required: true},//how to make this the same field
+  principal_entity: {type: String, required: true},
+  project_chief: { type: String, required: true },
 
-  description: {type: String},
-  worker_id: { type: String, required: true },
-  worker_name: {type: String},
+  added_entities: {type: String},
+  added_entity_director: {type: String},
+  added_entity_direction: {type: String},
+  added_entity_province: {type: String},
+  added_entity_phone: {type: Number},
+  added_entity_email: {type: String},
+
+  project_start: {type: Date},
+  project_end: {type: Date},
+
+  total_investment_cup: {type: Number, required: true},
+  total_investment_mlc: {type: Number},
+  financial_font_mlc: {type: String},
+  mlc_inverter: {type: String},
+  mlc_amount: {type: Number},
+  mlc_type: {type: String},
+
+  project_resume: {type: Text, required: true},
+  key_words: {type: Text},
+  problem_to_solve: {type: Text, required: true},
+
+  
   created_at: { type: Date, default: Date.now }
 });
 
